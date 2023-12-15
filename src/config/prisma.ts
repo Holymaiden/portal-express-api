89b9cc36db6f2edc018d67e6248081bb945bb8a2
Holymaiden <haikalwatampone3@gmail.com>
2023-12-15 23:54:33 +0800

    refactor(prisma): delete log

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,26 +1,12 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import config from "./config";
-import logger from "../utils/logger.utils";
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-const prismaClient: PrismaClient = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-  ],
-});
-
-type PrismaEvent = Prisma.QueryEvent | Prisma.LogEvent;
-
-prismaClient.$on("query", (e: PrismaEvent) => {
-  if ("params" in e) logger.info([e.query, e.params, e.duration + "ms"]);
-});
+const prismaClient: PrismaClient = new PrismaClient();
 
 if (config.NODE_ENV !== "production") globalThis.prisma = prismaClient;
 
